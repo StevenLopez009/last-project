278debab3352bfb1366b6a3efcca491df25c7d27
stevenLopez <stevenlopez009r@gmail.com>
2024-04-07 16:33:58 -0500

    nineth commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/sw.js").then(
+      function (registration) {
+        console.log("Service Worker registrado con éxito:", registration);
+      },
+      function (error) {
+        console.log("Error al registrar el Service Worker:", error);
+      }
+    );
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Isla JavaScript",
+        short_name: "IslaJS",
+        description: "Mi Progressive Web App",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "src/assets/img/icono.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "src/assets/img/icono.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
